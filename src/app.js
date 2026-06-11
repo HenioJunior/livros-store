a98c1a476d5734322171e926aeb30a9b822162fd
Henio Junior <henioajunior@gmail.com>
2026-06-11 13:01:16 -0300

    Adicionado a rota livros

--- src/app.js
+++ src/app.js
@@ -2,9 +2,39 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) =>{
-res.status(200).send("Curso de Node.js");
+const livros = [
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis"
+  },
+  {
+    id: 2,
+    titulo: "O Hobbit"
+  },
+  {
+    id: 3,
+    titulo: "Silmarillion"
+  },
+  {
+    id: 4,
+    titulo: "As Crônicas de Nárnia"
+  },
+  {
+    id: 5,
+    titulo: "Harry Potter e a Pedra Filosofal"
+  },
+  {
+    id: 6,
+    titulo: "Harry Potter e a Câmara Secreta"
+  }
+];
 
+app.get("/", (req, res) =>{
+res.status(200).send("Curso de Express/MongoDB");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
 });
 
 export default app;
